73b25322e342cf1f7faacdb2c23428118f44f782
lishayst100 <lishayst100@gmail.com>
2023-01-18 13:03:32 +0200

    finalproject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "react-toastify/dist/ReactToastify.css"
+import { Route,Routes } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Home from './components/Home';
+import Cart from './components/Cart';
+import NotFound from './components/NotFound';
+import {ToastContainer} from 'react-toastify'
+import PhoneDetails from './components/PhoneDetails';
+import About from './components/About';
+import CardList from './cardsPages/CardList';
+import EditCard from './cardsPages/EditCard';
+import Footer from './components/Footer';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products/:id' element={<PhoneDetails/>}/>
+        <Route path='/about/products/:id' element={<PhoneDetails/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/not-found' element={<NotFound/>}/>
+        <Route path='/cards' element={<CardList />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cards/edit/:id' element={<EditCard />} />
+      </Routes>
+      <ToastContainer/>
+      <Footer/>
+     
     </div>
   );
 }
